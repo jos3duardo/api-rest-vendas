ab38b98b4b6bca792d7845d41a2f9480f9f895fe
jsouza <jsouza@grupoimagetech.com.br>
2021-03-03 15:21:31 -0400

    servidor online

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,16 @@
-console.log('server')
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(routes)
+
+app.listen(3333, () => {
+    console.log('servidor rodando na porta 3333')
+})
+
+
