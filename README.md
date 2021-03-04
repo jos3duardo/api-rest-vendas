cadf65a63f41dcbfadeb871051417c20c961a96a
jos3duardo <jos3duardolopes@gmail.com>
2021-03-03 21:07:43 -0400

    personalizando erros

---
+++ README.md
@@ -0,0 +1,14 @@
+## API de Gestão de Vendas
+
+## Funcionalidades
+
+Cadastro de:
+ - Produtos
+ - Clientes
+ - Pedidos de Compra
+ - Gestão de Usuarios:
+    - Autenticação via token JWT
+    - Recuperação de senha por email
+    - atualização de perfil
+    - atualização de avatar
+
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,16 +1,32 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '../errors/AppError';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+           status: 'error',
+           message: error.message 
+        });
+    }
+    
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+});
+
 
 app.listen(3333, () => {
-    console.log('servidor rodando na porta 3333')
-})
+    console.log('servidor rodando na porta 3333');
+});
 
 
