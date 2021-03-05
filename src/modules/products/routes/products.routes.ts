32f55da5c69954b0536e8c688d55727fcbeec69d
jos3duardo <jos3duardolopes@gmail.com>
2021-03-04 22:56:37 -0400

    adicionando validação de campos nas rotas de produtos

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,57 @@
 import { Router } from 'express'
 import ProductsController from '../controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productsRoutes = Router();
 const productsController = new ProductsController()
 
 productsRoutes.get('/', productsController.index);
-productsRoutes.get('/:id', productsController.show);
-productsRoutes.post('/', productsController.create);
-productsRoutes.put('/:id', productsController.update);
-productsRoutes.delete('/:id', productsController.delete);
+
+productsRoutes.get(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        }
+    }),
+    productsController.show
+);
+
+productsRoutes.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+          name: Joi.string().required(),
+          price: Joi.number().precision(2).required(),
+          quantity: Joi.number().required()  
+        }
+    }),
+    productsController.create
+);
+
+productsRoutes.put(
+    '/:id',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().precision(2).required(),
+            quantity: Joi.number().required()
+        },
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        }
+    }),
+    productsController.update
+);
+
+productsRoutes.delete(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        }
+    }),
+    productsController.delete
+);
 
 export default productsRoutes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '../errors/AppError';
 import '@shared/typeorm';
@@ -12,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+//recebe os erros gerados pelo celebrate
+app.use(errors())
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
     if(error instanceof AppError){
