3190f23eb2fb95ff94db864b8fb2a3b25b12cf38
jos3duardo <jos3duardolopes@gmail.com>
2021-03-07 13:11:52 -0400

    criando template do email

--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -1,14 +1,33 @@
 import nodemailer from 'nodemailer';
-import { promises } from 'dns';
+import HandlebarsMailTemplate from './HandlebarsMailTemplate';
+
+interface IMailContact {
+    name: string,
+    enail: string,
+}
+
+interface ITemplateVariable {
+    [key: string]: string | number
+}
+
+interface IParseMailTemplate {
+    template: string,
+    variables: ITemplateVariable;
+} 
+
 
 interface ISendMail {
-    to: string,
-    body: string
+    to: IMailContact,
+    from?: IMailContact,
+    subject: string,
+    templateData: IParseMailTemplate
 }
 
 export default class EtherealMail {
-    static async sendMail({ to, body}: ISendMail): Promise<void> {
+    static async sendMail({ to, from, subject, templateData }: ISendMail): Promise<void> {
         const account = await nodemailer.createTestAccount()
+        
+        const mailTemplate = new HandlebarsMailTemplate();
         
         const transporter = nodemailer.createTransport({
             host: account.smtp.host,
@@ -21,10 +40,16 @@ export default class EtherealMail {
         });
         
         const message = await transporter.sendMail({
-            from: 'equipe@apivendas.com.br',
-            to,
-            subject: 'Recuperação de senha',
-            text: body,
+            from: {
+                name: from?.name,
+                address: from?.enail
+            },
+            to: {
+                name: to.name,
+                address: to.enail
+            },
+            subject,
+            html: await  mailTemplate.parse(templateData)
         })
         
         console.log('messagem sendo %s', message.messageId)
