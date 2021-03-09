2d633aeca3811b7f0fc5330e6b5899712f20a8ad
jos3duardo <jos3duardolopes@gmail.com>
2021-03-08 23:40:05 -0400

    criando entidade de orderns e colocando um await no update de avatar do usuario

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -15,7 +15,7 @@ class UpdateUserAvatarService {
     public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
         
         const userRepository = getCustomRepository(UserRepository);
-        const user = userRepository.findById(user_id);
+        const user = await userRepository.findById(user_id);
         
         if (!user) {
             throw new AppError('User not found');
